a93bc89386b5d209243ad30dc13ad9254ee1bc76
kvatoferma <jimmaggot@mail.ru>
2018-04-02 00:07:44 +0300

    Add some DOM logic in script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-let area = prompt('Enter area:');
+let area = 16; //prompt('Enter area:');
 let squareSide = 960 / area;
 let container = document.querySelector('.container');
 function createSquare() {
 	let square = document.createElement('div');
+	square.setAttribute('class', 'square');
 	square.style.border = '1px solid red';
 	square.style.height = squareSide +'px';
 	square.style.width = squareSide +'px';
@@ -11,6 +12,32 @@ function createSquare() {
 	return square;
 }
 for (let i = 0; i < area**2; i++) {
-	container.appendChild(createSquare());
+	let squareCreated = createSquare();
+	squareCreated.setAttribute('id', i);
+	container.appendChild(squareCreated);
 }
+
+/*
+let squares = document.getElementsByClassName("square");
+[].forEach.call(squares, (item) => {
+	item.addEventListener('mouseover', (e) => {
+		let id = e.target.getAttribute('id');
+	});
+
+		let squarePaint = item.getElementsById(id);
+		squarePaint.setAttribute('style', 'background-color:black')
+
 	
+});
+
+for (let j = 0; j < squares.length; j++) {
+	squares[j].addEventListener('mousemove', (e) => {
+		squares[j].setAttribute ('style','background-color: black');
+	}); */
+
+container.addEventListener('mouseover', (e) => {
+	let squareOverMouse = e.target.getElementsById();
+	squareOverMouse.setAttribute('style', 'background-color: black');
+});
+
+
