58c8c58f85108b3082dfd2b06e703434bf7331fb
kvatoferma <jimmaggot@mail.ru>
2018-04-03 15:57:06 +0300

    Edit script.js. Basic painting works!

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-let area = 16; //prompt('Enter area:');
+let area = 36; //prompt('Enter side length of the area:');
 let squareSide = 960 / area;
 let container = document.querySelector('.container');
-function createSquare() {
+function createSquare(id) {
 	let square = document.createElement('div');
 	square.setAttribute('class', 'square');
+	square.setAttribute('id', id);
 	square.style.border = '1px solid red';
 	square.style.height = squareSide +'px';
 	square.style.width = squareSide +'px';
@@ -12,32 +13,10 @@ function createSquare() {
 	return square;
 }
 for (let i = 0; i < area**2; i++) {
-	let squareCreated = createSquare();
-	squareCreated.setAttribute('id', i);
-	container.appendChild(squareCreated);
+	container.appendChild(createSquare(i));
 }
-
-/*
-let squares = document.getElementsByClassName("square");
-[].forEach.call(squares, (item) => {
-	item.addEventListener('mouseover', (e) => {
-		let id = e.target.getAttribute('id');
-	});
-
-		let squarePaint = item.getElementsById(id);
-		squarePaint.setAttribute('style', 'background-color:black')
-
-	
-});
-
-for (let j = 0; j < squares.length; j++) {
-	squares[j].addEventListener('mousemove', (e) => {
-		squares[j].setAttribute ('style','background-color: black');
-	}); */
-
 container.addEventListener('mouseover', (e) => {
-	let squareOverMouse = e.target.getElementsById();
-	squareOverMouse.setAttribute('style', 'background-color: black');
-});
-
-
+	let squareOverMouseId = e.target.getAttribute('id');
+	let squareToPaint = document.getElementById(squareOverMouseId);
+	squareToPaint.style.backgroundColor = 'black';
+	});
