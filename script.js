28cc393f96d660ccbb43eedf5314f78530dbff55
kvatoferma <jimmaggot@mail.ru>
2018-04-03 16:40:32 +0300

    Add random color painting.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let area = 36; //prompt('Enter side length of the area:');
+let area = 16; //prompt('Enter side length of the area:');
 let squareSide = 960 / area;
 let container = document.querySelector('.container');
 function createSquare(id) {
@@ -18,5 +18,36 @@ for (let i = 0; i < area**2; i++) {
 container.addEventListener('mouseover', (e) => {
 	let squareOverMouseId = e.target.getAttribute('id');
 	let squareToPaint = document.getElementById(squareOverMouseId);
-	squareToPaint.style.backgroundColor = 'black';
+	squareToPaint.style.backgroundColor = randomColor();
 	});
+
+function randomColor() {
+	let color = "#";
+	for (let i = 0; i < 6; i++) {
+		color += randomNumber();
+	}
+	return color;
+}
+
+function getRandomArbitrary(min, max) {
+	return Math.random() * (max - min) + min;
+}
+	
+function randomNumber() {
+	let num = Math.round(getRandomArbitrary (1,15));
+	switch (num) {
+		case 10: return 'A';
+		break;
+		case 11: return 'B';
+		break;
+		case 12: return 'C';
+		break;
+		case 13: return 'D';
+		break;
+		case 14: return 'E';
+		break;
+		case 15: return 'F';
+		break;
+		default: return num;
+	}
+}
