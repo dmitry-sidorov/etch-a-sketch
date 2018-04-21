e45bce704226c0ce54b3bf0a443686bcf1461454
kvatoferma <jimmaggot@mail.ru>
2018-04-21 16:47:49 +0300

    Edit

--- script.js
+++ script.js
@@ -1,83 +1,71 @@
-let area = 24; //prompt('Enter side length of the area:');
-
+//prompt('Enter side length of the area:');
+let defaultSquareAmount = 24;
+main(defaultSquareAmount);
 //let sideByDefault = true;
-let container = document.querySelector('.container');
+
 let btnClear = document.querySelector('.clear');
 let btnRainbow = document.querySelector('.rainbow');
 btnClear.addEventListener('click', (e) => {
-	//let sideByDefault = false;
-	area = prompt('Enter side length:');
+	let currentSquareAmount = prompt('Enter square amount per side:');
+	main(currentSquareAmount);
 });
 
-
-main(area);
-
-function main(area) {
+function main(squareAmount) {
+	let currentsideLength = 960 / squareAmount;
+	createArea(currentsideLength);
+	let container = document.querySelector('.container');
 	
-
-
-/*let area = 24;
-if (sideByDefault === false) {
-	area = prompt('Enter side length:');
-}*/
-let squareSide = 960 / area;
-createArea(area);
-
-
-
-
-
-function createArea(area) {
-	for (let i = 0; i < area**2; i++) {
-		container.appendChild(createSquare(i));
+	function createArea(sideLength) {
+		for (let i = 0; i < sideLength**2; i++) {
+			container.appendChild(createSquare(i, sideLength));
+		}
 	}
-}
-container.addEventListener('mouseover', (e) => {
-	let squareOverMouseId = e.target.getAttribute('id');
-	let squareToPaint = document.getElementById(squareOverMouseId);
-	squareToPaint.style.backgroundColor = randomColor();
+	container.addEventListener('mouseover', (e) => {
+		let squareOverMouseId = e.target.getAttribute('id');
+		let squareToPaint = document.getElementById(squareOverMouseId);
+		squareToPaint.style.backgroundColor = randomColor();
 	});
 
-function createSquare(id) {
-	let square = document.createElement('div');
-	square.setAttribute('class', 'square');
-	square.setAttribute('id', id);
-	square.style.border = '1px solid red';
-	square.style.height = squareSide +'px';
-	square.style.width = squareSide +'px';
-	square.style.float = 'left';
-	square.style.boxSizing = 'border-box';
-	return square;
-}
-
-function randomColor() {
-	let color = "#";
-	for (let i = 0; i < 6; i++) {
-		color += randomNumber();
+	function createSquare(id, sideLength) {
+		let square = document.createElement('div');
+		square.setAttribute('class', 'square');
+		square.setAttribute('id', id);
+		square.style.border = '1px solid red';
+		square.style.height = sideLength +'px';
+		square.style.width = sideLength +'px';
+		square.style.float = 'left';
+		square.style.boxSizing = 'border-box';
+		return square;
 	}
-	return color;
-}
 
-function getRandomArbitrary(min, max) {
-	return Math.random() * (max - min) + min;
-}
+	function randomColor() {
+		let color = "#";
+		for (let i = 0; i < 6; i++) {
+			color += randomNumber();
+		}
+		return color;
+	}
+
+	function getRandomArbitrary(min, max) {
+		return Math.random() * (max - min) + min;
+	}
 	
-function randomNumber() {
-	let num = Math.round(getRandomArbitrary (1,15));
-	switch (num) {
-		case 10: return 'A';
-		break;
-		case 11: return 'B';
-		break;
-		case 12: return 'C';
-		break;
-		case 13: return 'D';
-		break;
-		case 14: return 'E';
-		break;
-		case 15: return 'F';
-		break;
-		default: return num;
+	function randomNumber() {
+		let num = Math.round(getRandomArbitrary (1,15));
+		switch (num) {
+			case 10: return 'A';
+			break;
+			case 11: return 'B';
+			break;
+			case 12: return 'C';
+			break;
+			case 13: return 'D';
+			break;
+			case 14: return 'E';
+			break;
+			case 15: return 'F';
+			break;
+			default: return num;
+		}
 	}
-}
 }
